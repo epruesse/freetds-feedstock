7cb948aa866c1bc34382c576297baa1ffc0fe055
Marius Van Niekerk <marius.v.niekerk@gmail.com>
2017-09-02 13:06:39 -0400

    Update recipe to include unixodbc and ssl linkage

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-./configure --prefix $PREFIX
+./configure --prefix $PREFIX --with-unixodbc=$PREFIX --with-openssl=$PREFIX
 make
 # To run this check we need to have access to a mssql instance.
 # make check
