866ff1ddbda24e3248bce42e589495c2d5106019
Uwe L. Korn <uwe.korn@quantco.com>
2021-06-04 17:14:13 +0200

    Skip odbc_config when cross-compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,11 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
 set -e
 
+if [[ "${build_platform}" != "${target_platform}" ]]; then
+  # Don't try to extract information from the odbc_config executable
+  rm $PREFIX/bin/odbc_config
+fi
+
 ./configure --prefix $PREFIX --with-unixodbc=$PREFIX --with-openssl=$PREFIX
 make
 # To run this check we need to have access to a mssql instance.
