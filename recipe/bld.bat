5c19fa3bb2ba1de5aa360bc706b6a0c7858bf019
Uwe L. Korn <uwe.korn@quantco.com>
2021-06-11 14:10:06 +0200

    Build on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,10 @@
 
 set OPENSSL_ROOT_DIR=%LIBRARY_PREFIX%
 
-set CMAKE_GENERATOR="NMake Makefiles JOM"
 mkdir build
 cd build
 
-cmake -G "%CMAKE_GENERATOR%" ^
+cmake -GNinja ^
          -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
          -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
          -DCMAKE_BUILD_TYPE=Release ^
